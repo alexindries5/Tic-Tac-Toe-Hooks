1e23245a7e4cd20db865b137dd129dad3fb6e838
Alex <alexandru.indries@emanuel.ro>
2020-12-17 20:28:05 +0200

    Done!

---
+++ src/Square.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Square({value=[],onClick}) {
+    return (
+      <div className="square" onClick={onClick}>
+        {value}
+      </div>
+    );
+}
+
+
+
+export default Square;
